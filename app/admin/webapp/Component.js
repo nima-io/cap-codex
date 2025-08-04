a3b6f27f8bb54fe074c2d2167a69113eab93e625
nima-io <nimahassaniasl@gmail.com>
2025-08-04 18:48:10 +0200

    fix: load admin fiori app

--- app/admin/webapp/Component.js
+++ app/admin/webapp/Component.js
@@ -1,9 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent"
-], function (UIComponent) {
+    "sap/fe/core/AppComponent"
+], function (AppComponent) {
     "use strict";
 
-    return UIComponent.extend("admin.Component", {
+    return AppComponent.extend("admin.Component", {
         metadata: {
             manifest: "json"
         }
