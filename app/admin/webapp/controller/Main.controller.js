410609c2f8b7845bff796fe94d4d91d348a6fbaa
nima-io <nimahassaniasl@gmail.com>
2025-08-05 14:24:52 +0200

    feat(admin): add book management with CRUD

--- app/admin/webapp/controller/Main.controller.js
+++ app/admin/webapp/controller/Main.controller.js
@@ -6,6 +6,15 @@ sap.ui.define([
   return Controller.extend("admin.controller.Main", {
     onSave: function() {
       this.getView().getModel().submitBatch("$auto");
+    },
+
+    onBookPress: function(oEvent) {
+      const id = oEvent.getSource().getBindingContext().getProperty("ID");
+      this.getOwnerComponent().getRouter().navTo("bookDetail", { ID: id });
+    },
+
+    onAddBook: function() {
+      this.getOwnerComponent().getRouter().navTo("bookCreate");
     }
   });
 });
