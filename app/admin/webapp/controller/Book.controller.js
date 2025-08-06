a480b9004c154aca4edaeeffc3d14ec37fa0902f
nima-io <nimahassaniasl@gmail.com>
2025-08-06 14:33:45 +0200

    refactor: let cap handle book ids

--- app/admin/webapp/controller/Book.controller.js
+++ app/admin/webapp/controller/Book.controller.js
@@ -3,16 +3,6 @@ sap.ui.define([
 ], function(Controller) {
   "use strict";
 
-  function generateUUID() {
-    return (typeof crypto !== "undefined" && crypto.randomUUID)
-      ? crypto.randomUUID()
-      : "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-          const r = Math.random() * 16 | 0;
-          const v = c === "x" ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
-        });
-  }
-
   return Controller.extend("admin.controller.Book", {
     onInit: function() {
       const oRouter = this.getOwnerComponent().getRouter();
@@ -29,7 +19,6 @@ sap.ui.define([
       const oModel = this.getView().getModel();
       const oListBinding = oModel.bindList("/Books");
       const oContext = oListBinding.create({
-        ID: generateUUID(),
         IsActiveEntity: true
       });
       this.getView().setBindingContext(oContext);
--- srv/admin-service.js
+++ srv/admin-service.js
@@ -5,9 +5,6 @@ module.exports = cds.service.impl(function () {
   const { Returns } = this.entities;
 
   this.before(['CREATE', 'UPDATE'], 'Books', (req) => {
-    if (req.event === 'CREATE' && !req.data.ID) {
-      req.data.ID = cds.utils.uuid();
-    }
     if (req.data.price !== undefined) {
       const value = typeof req.data.price === 'string' ? parseFloat(req.data.price) : req.data.price;
       if (!Number.isNaN(value)) req.data.price = value;
@@ -24,14 +21,12 @@ module.exports = cds.service.impl(function () {
   this.on('requestReturn', 'OrderItems', async (req) => {
     const { reason } = req.data;
     const { parent_ID, lineNo } = req.params[0];
-    const ID = cds.utils.uuid();
-    await INSERT.into(Returns).entries({
-      ID,
+    const inserted = await INSERT.into(Returns).entries({
       orderItem_parent_ID: parent_ID,
       orderItem_lineNo: lineNo,
       reason,
       status: 'REQUESTED'
     });
-    return SELECT.one.from(Returns).where({ ID });
+    return SELECT.one.from(Returns).where({ ID: inserted.ID });
   });
 });
